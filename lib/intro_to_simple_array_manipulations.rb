d1e71b8ee21436bceec1ec67c02ea10d5eb98d2b
Manoj Patel <nycdays300@gmail.com>
2020-01-25 02:45:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,5 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 colors_in_the_rainbow.push("violet")
+
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+bouroughs_in_ny.unshift("Staten Island")
